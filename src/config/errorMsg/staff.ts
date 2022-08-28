e0b1858cbf9c7797f0b7c0137b6adc70e2c52e8a
Huang Daoxu <34485325+sanjayheaven@users.noreply.github.com>
2022-08-28 13:54:55 +0800

    test husky

--- src/config/errorMsg/staff.ts
+++ src/config/errorMsg/staff.ts
@@ -4,6 +4,14 @@ export const UsernameShouldBeUnique: IErrorMsg = {
   msg: "Username should be unique.",
   code: 2001,
 }
+
+
+
+
+
+
+
+
 export const StaffNotExist = {
   msg: "Staff does't exist.",
   code: 2002,
